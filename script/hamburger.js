e8b9171b112b6823bf14c96b6c09f8d07e4be7e4
Nikolay Kolyshev <sword.of.destiny2001@mail.ru>
2020-07-31 00:04:36 +0300

    sections: intro, about

---
+++ script/hamburger.js
@@ -0,0 +1,23 @@
+$(function() {
+    
+    let hamburger = $('#hamburger'),
+    isClosed = false;
+
+    hamburger.click(function () {
+        burgerTime();
+    });
+
+    function burgerTime() {
+      if (isClosed == true) {
+        hamburger.removeClass('is-open');
+        hamburger.addClass('is-closed');
+        isClosed = false;
+      }
+      else {
+        hamburger.removeClass('is-closed');
+        hamburger.addClass('is-open');
+        isClosed = true;
+      }
+    }
+
+  });
--- script/script.js
+++ script/script.js
@@ -1,9 +1,30 @@
-window.onload = () =>{
+window.onload = ()=> {
 
+      
+
+    // DOM-elements  
+  
     const stickyHeader = document.querySelector(".main_header");
     const stickyContact = $(".nav_contact");
     const stickyLogin = document.querySelector(".header_login");
     const stickyAside = document.querySelector(".header_aside");
+    const stickyPages = document.querySelector(".nav_pages");
+
+    const aboutNavbarItems = document.querySelectorAll(".about_all_navbar_item");
+
+    // Section-Intro: Start svg-img Animation
+
+    let svgStart = new Walkway ({
+      selector: "#univesity_image",
+      duration: "2500",
+      easing: "easeOutQuint"
+    });
+    svgStart.draw((()=> {
+        document.querySelector("#univesity_image").classList.add("university_image_end_animation");
+      })());
+
+
+    // Header: Event Position Sticky
 
     const option = {
         root: null,
@@ -19,6 +40,7 @@ window.onload = () =>{
             stickyContact.slideUp(300);
             stickyLogin.classList.add("header_login_sticky");
             stickyAside.classList.add("header_aside_sticky");
+            stickyPages.classList.add("nav_pages_sticky");
           }
           else
           {
@@ -26,6 +48,7 @@ window.onload = () =>{
             stickyContact.slideDown(300);
             stickyLogin.classList.remove("header_login_sticky");
             stickyAside.classList.remove("header_aside_sticky");
+            stickyPages.classList.remove("nav_pages_sticky");
           }
       });
     }
@@ -33,5 +56,47 @@ window.onload = () =>{
     const observer = new IntersectionObserver(callback, option);
 
     observer.observe(stickyHeader);
+
+
+  // Section-About: Circle animation
+
+  function Circle(el){
+    $(el).circleProgress({
+      fill: { color: "#330066" },
+      emptyFill: "white",
+      lineCap: "round",
+      startAngle: -1.57,
+      size: 346,
+      animationStartValue: 0.35,
+      insertMode: "append"
+    })
+    .on("circle-animation-progress", function(event, progress, stepValue){
+      $(this).find(".about_job_percentage .percentage_value")
+      .text(parseInt(String(stepValue.toFixed(2)).substr(2))+"%")
+    });
+  }
+
+  Circle(".about_job");
+
+
+
+  // Section-About: Navbar
+
+  aboutNavbarItems.forEach((item)=>{
+    item.onclick = function(){
+
+      if (item.classList.contains("about_all_navbar_item_active"))
+        return;
+
+      for (element of aboutNavbarItems)
+        element.classList.remove("about_all_navbar_item_active");
+      item.classList.add("about_all_navbar_item_active");
+      
+    }
+  });
+
+
+
+
 
 }
