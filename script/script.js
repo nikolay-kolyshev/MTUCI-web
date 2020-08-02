6926219cdcd74b7b7fdfd082c4bcdd5bfb793af3
Nikolay Kolyshev <sword.of.destiny2001@mail.ru>
2020-08-02 21:25:58 +0300

    section science

--- script/script.js
+++ script/script.js
@@ -12,6 +12,119 @@ window.onload = ()=> {
 
     const aboutNavbarItems = document.querySelectorAll(".about_all_navbar_item");
 
+
+     // Section-About: Circle animation
+
+    class CircleIcon {
+
+      values = new Map ([
+        [
+          "teachers",
+          {
+            name: "Квалифицированных<br/>преподавателей",
+            numb: 450,
+            symbol: "+",
+            percent: 1,
+            description: `Наши преподаватели - это те люди,
+                          которые смогут обучить вас той профессии,
+                          в которой вы видите себя в будующем`
+          }
+        ],
+        [
+          "budget",
+          {
+            name: "Бюджетных мест<br/>для абитуриентов",
+            numb: 180,
+            symbol: "+",
+            percent: 1,
+            description: `Для каждого способного студента у
+                          нас есть высокие стипендии,
+                          а также места на бюджетной основе.`
+          }
+        ],
+        [
+          "education",
+          {
+            name: "Образовательных<br/>программ",
+            numb: 60,
+            symbol: "+",
+            percent: 1,
+            description: `Наличие огромного количества направлений,
+                          позволит вам индивидуально подобрать курс.`
+          }
+        ],
+        [
+          "dormitory",
+          {
+            name: "Свободных мест<br/>в общежитии",
+            numb: 1200,
+            symbol: "+",
+            percent: 1,
+            description: `Наши общежития оборудованы и
+                          готовы к эксплуатации студентами`
+          }
+        ],
+        [
+          "employment",
+          {
+            name: "Трудоустройство<br/>после вуза",
+            numb: 85,
+            symbol: "%",
+            percent: 0.85,
+            description: `Наши студенты имеют отличную 
+                          репутацию среди работодателей 
+                          благодаря знаниям, полученным у нас.`
+          }
+        ]
+      ])
+    
+      GetValue(valueName, args = null){
+        if (args != null){
+    
+          if (this.values.has(valueName)){
+            return;
+          }
+    
+          this.values.set(valueName, args);
+        
+        }
+        let value = this.values.get(valueName);
+        this.CreateCircle(value);
+      }
+    
+      CreateCircle(value){
+    
+        (function Circle(el){
+    
+          $(el).circleProgress({
+            value: value.percent,
+            fill: { color: "#330066" },
+            emptyFill: "white",
+            lineCap: "round",
+            startAngle: -1.57,
+            size: 346,
+            animationStartValue: 0.35,
+            insertMode: "append"
+          })
+          .on("circle-animation-progress", function(event, progress, stepValue){
+    
+            $(this).find(".about_more_icon_inner_percentage .percentage_value")
+              .text(parseInt((parseFloat(stepValue.toFixed(2)) * (1/value.percent*value.numb))) + value.symbol);
+    
+            $(this).find(".about_more_icon_inner_percentage .percentage_name")
+              .html(value.name);
+          
+            });
+    
+        })(".about_more_icon_inner");
+      
+        $(".about_more_description > .description_text").text(value.description);
+    
+      }
+    
+    }
+    
+
     // Section-Intro: Start svg-img Animation
 
     let svgStart = new Walkway ({
@@ -58,30 +171,12 @@ window.onload = ()=> {
     observer.observe(stickyHeader);
 
 
-  // Section-About: Circle animation
-
-  function Circle(el){
-    $(el).circleProgress({
-      fill: { color: "#330066" },
-      emptyFill: "white",
-      lineCap: "round",
-      startAngle: -1.57,
-      size: 346,
-      animationStartValue: 0.35,
-      insertMode: "append"
-    })
-    .on("circle-animation-progress", function(event, progress, stepValue){
-      $(this).find(".about_job_percentage .percentage_value")
-      // .text(parseInt(String(stepValue.toFixed(2)).substr(2))+"%")
-      .text((parseFloat(stepValue.toFixed(2))) + "+")
-    });
-  }
-
-  Circle(".about_job");
-
-
 
   // Section-About: Navbar
+
+  const CircleIconStandart = new CircleIcon();
+  CircleIconStandart.GetValue("employment");
+
 
   aboutNavbarItems.forEach((item)=>{
     item.onclick = function(){
@@ -91,6 +186,7 @@ window.onload = ()=> {
 
       for (element of aboutNavbarItems)
         element.classList.remove("about_all_navbar_item_active");
+      CircleIconStandart.GetValue(item.id);
       item.classList.add("about_all_navbar_item_active");
       
     }
@@ -98,6 +194,63 @@ window.onload = ()=> {
 
 
 
-
+  particlesJS("particles_js", {
+    particles: {
+      number: {
+        value: 50,
+        density: { enable: true, value_area: 789.1850086415761 }
+      },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#ffffff" },
+        polygon: { nb_sides: 6 },
+        image: { src: "img/github.svg", width: 100, height: 100 }
+      },
+      opacity: {
+        value: 0.5528314585136049,
+        random: true,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      },
+      size: {
+        value: 5,
+        random: false,
+        anim: { enable: false, speed: 0, size_min: 0, sync: true }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.6569881101176175,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 4,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "bounce",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+        resize: true
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 446.3379995942385, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 }
+      }
+    },
+    retina_detect: true
+  });
 
 }
